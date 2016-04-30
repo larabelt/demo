01a585155b9c869d21eae7561979e22e6232cf3e
Matt Crowe <matthew.p.crowe@gmail.com>
2016-04-30 13:08:05 -0400

    users table

--- database/migrations/2016_04_30_000000_ohio_create_users_table.php
+++ database/migrations/2016_04_30_000000_ohio_create_users_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class OhioCreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->boolean('is_active')->default(1);
+            $table->boolean('is_verified')->default(0);
             $table->string('email')->unique();
+            $table->string('first_name', 100)->nullable();
+            $table->string('last_name', 100)->nullable();
+            $table->string('mi', 1)->nullable();
+            $table->string('username', 100)->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
