52cf27692621d09234fc7025d4514f261d099714
Matt Crowe <matthew.p.crowe@gmail.com>
2016-07-20 20:31:57 -0400

    refactor gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,89 +1,70 @@
+var concat = require('gulp-concat');
 var gulp = require('gulp');
 var include = require('gulp-include');
-var elixir = require('laravel-elixir');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+function copy_files(input, output) {
+    gulp.src(input).pipe(gulp.dest(output));
+}
 
-function mixWithIncludes(source, target) {
-    gulp.src(source)
+function mix_sass(input, output) {
+    return gulp
+        .src(input)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(output));
+}
+
+function mix_js(input, output) {
+    gulp.src(input)
         .pipe(include())
         .on('error', console.log)
-        .pipe(gulp.dest(target));
+        .pipe(gulp.dest(output));
 }
 
-function copy(source, target) {
-    gulp.src(source).pipe(gulp.dest(target));
-}
-
-gulp.task('sass', function () {
-    elixir(function(mix) {
-        console.info('mixing sass files');
-        copy('./bower_components/fontawesome/fonts/**/*', './public/fonts/');
-        mix.sass('admin.scss');
-    });
+gulp.task('copy', function () {
+    copy_files('./node_modules/angular-ui-bootstrap/template/**/*', './public/ng/vendor/angular-ui-bootstrap/template/');
 });
 
-gulp.task('vendor-copy', function () {
-    console.info('copying font files');
-    copy('./bower_components/fontawesome/fonts/**/*', './public/fonts/');
-    copy('./bower_components/bootstrap-sass-official/assets/fonts/**/*', './public/fonts/');
-    copy('./node_modules/angular-ui-bootstrap/template/**/*', './public/ng/vendor/angular-ui-bootstrap/template/');
+gulp.task('sass', function () {
+    mix_sass('./resources/assets/sass/admin.scss', './public/css');
+});
+
+gulp.task('ng', function () {
+    copy_files('./vendor/ohiocms/admin/resources/assets/ng/base/views/**/*', './public/ng/base/views');
+    copy_files('./vendor/ohiocms/admin/resources/assets/ng/users/views/**/*', './public/ng/users/views');
+    mix_js(['./vendor/ohiocms/admin/resources/assets/ng/users/app.js'], './public/ng/users', 'app');
 });
 
 gulp.task('js', function () {
-    elixir(function(mix) {
-        console.info('mixing js files');
-        // Admin JS Head Library
-        mix.scripts([
-            'bower_components/angular/angular.js',
-            'bower_components/angular-route/angular-route.js',
-            'node_modules/angular-ui-bootstrap/dist/ui-bootstrap.js',
-        ], 'public/js/admin-head-lib.js', './');
-        // Admin JS Footer Library
-        mix.scripts([
-            'bower_components/jquery/dist/jquery.min.js',
-            'bower_components/jquery-ui/jquery-ui.min.js',
-            'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.min.js',
-            'bower_components/AdminLTE/plugins/morris/morris.min.js',
-            'bower_components/AdminLTE/plugins/sparkline/jquery.sparkline.min.js',
-            'bower_components/AdminLTE/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js',
-            'bower_components/AdminLTE/plugins/jvectormap/jquery-jvectormap-world-mill-en.js',
-            'bower_components/AdminLTE/plugins/knob/jquery.knob.js',
-            'bower_components/AdminLTE/plugins/daterangepicker/daterangepicker.js',
-            'bower_components/AdminLTE/plugins/datepicker/bootstrap-datepicker.js',
-            'bower_components/AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js',
-            'bower_components/AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js',
-            'bower_components/AdminLTE/plugins/fastclick/fastclick.js',
-            'bower_components/AdminLTE/dist/js/app.min.js',
-            'bower_components/AdminLTE/dist/js/pages/dashboard.js',
-            'bower_components/AdminLTE/dist/js/demo.js'
-        ], 'public/js/admin-footer-lib.js', './');
-    });
+
+    //mix_js(['./node_modules/angular-ui-bootstrap/dist/ui-bootstrap.js'], './public/js/', 'admin-head-lib');
+
+    mix_js([
+        './bower_components/AdminLTE/plugins/sparkline/jquery.sparkline.min.js',
+        './bower_components/AdminLTE/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js',
+        './bower_components/AdminLTE/plugins/jvectormap/jquery-jvectormap-world-mill-en.js',
+        './bower_components/AdminLTE/plugins/knob/jquery.knob.js',
+        './bower_components/AdminLTE/plugins/daterangepicker/daterangepicker.js',
+        './bower_components/AdminLTE/plugins/datepicker/bootstrap-datepicker.js',
+        './bower_components/AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js',
+        './bower_components/AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js',
+        './bower_components/AdminLTE/plugins/fastclick/fastclick.js',
+        './bower_components/AdminLTE/dist/js/app.min.js',
+        './bower_components/AdminLTE/dist/js/pages/dashboard.js',
+        './bower_components/AdminLTE/dist/js/demo.js'
+    ], './public/js/', 'admin-footer-lib');
+
 });
 
-gulp.task('angular', function () {
-    console.info('mixing angular files');
-    copy('./vendor/ohiocms/admin/resources/assets/ng/base/views/**/*', './public/ng/base/views');
-    copy('./vendor/ohiocms/admin/resources/assets/ng/users/views/**/*', './public/ng/users/views');
-    mixWithIncludes('./vendor/ohiocms/admin/resources/assets/ng/users/app.js', './public/ng/users');
-});
+gulp.task('default', ['copy', 'sass', 'ng', 'js']);
 
-gulp.task('watch-angular', function () {
-    gulp.watch('vendor/ohiocms/admin/resources/assets/ng/**/*', ['angular', 'js']);
+gulp.task('watch', function () {
+    gulp.watch('./vendor/ohiocms/admin/resources/assets/ng/**/*', ['ng']);
+    gulp.watch('./resources/assets/sass/**/*', ['sass']);
+    gulp.watch('./vendor/ohiocms/admin/resources/assets/sass/**/*', ['sass']);
 });
-
-gulp.task('watch-sass', function () {
-    gulp.watch('vendor/ohiocms/admin/resources/assets/sass/**/*', ['sass']);
-});
-
-gulp.task('default', ['sass', 'vendor-copy', 'js', 'angular']);
-gulp.task('watch', ['watch-angular', 'watch-sass']);
--- readme.md
+++ readme.md
@@ -26,4 +26,4 @@ If you discover a security vulnerability within Laravel, please send an e-mail t
 
 The Laravel framework is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT).
 
-# php artisan cache:clear; sudo service php5-fpm restart;
+# sudo service php5-fpm restart;
