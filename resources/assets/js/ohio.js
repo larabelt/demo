9d88681c9ed2dda524b22af38bb760068494129a
Matt Crowe <matthew.p.crowe@gmail.com>
2016-11-04 17:13:14 -0400

    minor vue change. ohiocms composer update

--- resources/assets/js/ohio.js
+++ resources/assets/js/ohio.js
@@ -3,16 +3,19 @@ window._ = require('lodash');
 window.Vue = require('vue');
 window.VueRouter = require('vue-router');
 
-import  OhioCore  from '../../ohio/core/js/app';
+import  OhioCore  from '../../ohio/core/js/core';
+import  OhioContent  from '../../ohio/content/js/content';
+import  HelloPlugin  from '../../ohio/core/js/plugins/hello';
 
 Vue.use(require('vue-resource'));
 Vue.use(VueRouter);
+Vue.use(HelloPlugin); //test...
 Vue.config.devtools = true;
-//import  Content  from '../../../ohio/content/js/component';
 
 $(document).ready(function() {
 
-    let core = new OhioCore([
-        //Content
+    new OhioCore([
+        OhioContent
     ]);
+
 });
