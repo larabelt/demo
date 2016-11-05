ff2f13a7d826e9f50563825e8751d19b337d3932
rob.lasota <rob.lasota@fahlgren.com>
2016-11-05 10:47:23 -0400

    updated relative import paths

--- resources/assets/js/ohio.js
+++ resources/assets/js/ohio.js
@@ -1,11 +1,11 @@
+import  OhioCore  from 'ohio/core/js/core';
+import  OhioContent  from 'ohio/content/js/content';
+import  HelloPlugin  from 'ohio/core/js/plugins/hello';
+
 window.$ = window.jQuery = require('jquery');
-window._ = require('lodash');
+global._ = require('lodash');
 window.Vue = require('vue');
 window.VueRouter = require('vue-router');
-
-import  OhioCore  from '../../ohio/core/js/core';
-import  OhioContent  from '../../ohio/content/js/content';
-import  HelloPlugin  from '../../ohio/core/js/plugins/hello';
 
 Vue.use(require('vue-resource'));
 Vue.use(VueRouter);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
+let path = require('path');
+
 module.exports = {
 
     resolve: {
         alias: {
             'vue$': 'vue/dist/vue.js'
-        }
+        },
+        modules: [
+            path.resolve('./resources'),
+            path.resolve('./resources/assets/js'),
+            path.resolve('./node_modules')
+        ]
     }
 
 }
