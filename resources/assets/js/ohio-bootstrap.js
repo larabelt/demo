3f874b48733350100ab8d21cb851aff10c7e9e08
Matt Crowe <matthew.p.crowe@gmail.com>
2016-11-08 19:57:33 -0500

    vue stuff

--- resources/assets/js/ohio-bootstrap.js
+++ resources/assets/js/ohio-bootstrap.js
@@ -6,3 +6,8 @@ window.VueRouter = require('vue-router');
 Vue.use(require('vue-resource'));
 Vue.use(VueRouter);
 Vue.config.devtools = true;
+
+import columnSorter from 'ohio/core/js/components/base/column-sorter';
+import pagination from 'ohio/core/js/components/base/pagination';
+Vue.component('column-sorter', columnSorter);
+Vue.component('pagination', pagination);
