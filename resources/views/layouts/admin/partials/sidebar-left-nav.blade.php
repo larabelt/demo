ba124d6ecfcef860486acd2f495f9221f56fb221
Matt Crowe <matthew.p.crowe@gmail.com>
2016-12-21 14:15:30 -0500

    nav menu file

---
+++ resources/views/layouts/admin/partials/sidebar-left-nav.blade.php
@@ -0,0 +1,11 @@
+<li class="header">MAIN NAVIGATION</li>
+@include('ohio-core::layouts.admin.partials.sidebar-left-nav')
+@include('ohio-content::layouts.admin.partials.sidebar-left-nav')
+<li class="treeview">
+    <a href="#">
+        <i class="fa fa-th"></i> <span>Widget Admin</span> <i class="fa fa-angle-left pull-right"></i>
+    </a>
+    <ul class="treeview-menu">
+        <li><a href="/admin/widgets"><i class="fa fa-th"></i> <span>Widgets</span></a></li>
+    </ul>
+</li>
