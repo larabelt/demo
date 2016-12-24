4555d537d8a31ff00bfba38c2c2a8f67fb2c247a
Matt Crowe <matthew.p.crowe@gmail.com>
2016-12-24 13:45:20 -0500

    composer update

--- resources/views/layouts/admin/partials/sidebar-left-nav.blade.php
+++ resources/views/layouts/admin/partials/sidebar-left-nav.blade.php
@@ -3,9 +3,9 @@
 @include('ohio-content::layouts.admin.partials.sidebar-left-nav')
 <li class="treeview">
     <a href="#">
-        <i class="fa fa-th"></i> <span>Widget Admin</span> <i class="fa fa-angle-left pull-right"></i>
+        <i class="fa fa-cog"></i> <span>Widget Admin</span> <i class="fa fa-angle-left pull-right"></i>
     </a>
     <ul class="treeview-menu">
-        <li><a href="/admin/widgets"><i class="fa fa-th"></i> <span>Widgets</span></a></li>
+        <li><a href="/admin/widgets"><i class="fa fa-cogs"></i> <span>Widgets</span></a></li>
     </ul>
 </li>
