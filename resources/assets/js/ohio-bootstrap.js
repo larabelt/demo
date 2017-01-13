532bb3f6e10556b24c1962e7905addc172f92794
Matt Crowe <matthew.p.crowe@gmail.com>
2017-01-13 16:01:03 -0500

    composer update

--- resources/assets/js/ohio-bootstrap.js
+++ resources/assets/js/ohio-bootstrap.js
@@ -1,13 +1,14 @@
 window.$ = window.jQuery = require('jquery');
 global._ = require('lodash');
 window.Vue = require('vue');
+window.Vuex = require('vuex');
 window.VueRouter = require('vue-router');
+import columnSorter from 'ohio/core/js/components/base/column-sorter';
+import pagination from 'ohio/core/js/components/base/pagination';
 
 Vue.use(require('vue-resource'));
 Vue.use(VueRouter);
 Vue.config.devtools = true;
 
-import columnSorter from 'ohio/core/js/components/base/column-sorter';
-import pagination from 'ohio/core/js/components/base/pagination';
 Vue.component('column-sorter', columnSorter);
 Vue.component('pagination', pagination);
--- resources/assets/js/ohio-core.js
+++ resources/assets/js/ohio-core.js
@@ -3,7 +3,5 @@ require('./ohio-bootstrap');
 import  OhioCore  from 'ohio/core/js/core';
 
 $(document).ready(function() {
-
     new OhioCore();
-
 });
