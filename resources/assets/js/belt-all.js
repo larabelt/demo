f7a326eb4588d8d59bbafd77884680efd4030bbd
Matt Crowe <github.mpc@zym.me>
2017-02-16 21:14:25 +0000

    more clip refactor

--- resources/assets/js/belt-all.js
+++ resources/assets/js/belt-all.js
@@ -2,7 +2,7 @@ import './belt-bootstrap';
 
 import  BeltCore  from 'belt/core/js/core';
 import  BeltContent  from 'belt/content/js/content';
-import  BeltSpot  from 'belt/clip/js/spot';
+import  BeltSpot  from 'belt/spot/js/spot';
 import  BeltClip  from 'belt/clip/js/clip';
 
 $(document).ready(function() {
