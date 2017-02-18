edb03068cb2941f91f23cd433aa7c623f4f75493
Matt Crowe <github.mpc@zym.me>
2017-02-18 13:59:08 +0000

    minor cleanup

--- resources/views/archive/attachments/sections/default.blade.php
+++ resources/views/archive/attachments/sections/default.blade.php
@@ -5,6 +5,6 @@
 <div class="section section-attachment {{ $section->param('class') }}">
     @include('sections.sections._header')
     @include('sections.sections._body')
-    @include('attachments.web._show')
+    @include('attachments.web.show')
     @include('sections.sections._footer')
 </div>
--- resources/views/archive/blocks/sections/default.blade.php
+++ resources/views/archive/blocks/sections/default.blade.php
@@ -5,6 +5,6 @@
 <div class="section section-block {{ $section->param('class') }}">
     @include('belt-content::sections.sections._header')
     @include('belt-content::sections.sections._body')
-    @include('belt-content::blocks.web._show')
+    @include('belt-content::blocks.web.show')
     @include('belt-content::sections.sections._footer')
 </div>
