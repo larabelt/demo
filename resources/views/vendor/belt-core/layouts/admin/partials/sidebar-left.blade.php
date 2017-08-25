c8b1b4274b035672acdb8344ef11c55f368df9a9
Matt Crowe <github.mpc@zym.me>
2017-08-25 18:04:12 +0000

    tweak admin tab content areas to be more consistent

--- resources/views/vendor/belt-core/layouts/admin/partials/sidebar-left.blade.php
+++ resources/views/vendor/belt-core/layouts/admin/partials/sidebar-left.blade.php
@@ -4,11 +4,11 @@
 @include('belt-spot::layouts.admin.partials.sidebar-left-nav')
 @include('belt-clip::layouts.admin.partials.sidebar-left-nav')
 @include('belt-menu::layouts.admin.partials.sidebar-left-nav')
-<li class="treeview">
-    <a href="#">
-        <i class="fa fa-cog"></i> <span>Widget Admin</span> <i class="fa fa-angle-left pull-right"></i>
-    </a>
-    <ul class="treeview-menu">
-        <li><a href="/admin/widgets"><i class="fa fa-cogs"></i> <span>Widgets</span></a></li>
-    </ul>
-</li>
+{{--<li class="treeview">--}}
+    {{--<a href="#">--}}
+        {{--<i class="fa fa-cog"></i> <span>Widget Admin</span> <i class="fa fa-angle-left pull-right"></i>--}}
+    {{--</a>--}}
+    {{--<ul class="treeview-menu">--}}
+        {{--<li><a href="/admin/widgets"><i class="fa fa-cogs"></i> <span>Widgets</span></a></li>--}}
+    {{--</ul>--}}
+{{--</li>--}}
