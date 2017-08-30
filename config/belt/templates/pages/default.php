6f8d1d41a76e700636aab6ef97f6be60d1d9f1e6
Matt Crowe <github.mpc@zym.me>
2017-08-30 20:28:47 +0000

    paramable pages. paramable bug

--- config/belt/templates/pages/default.php
+++ config/belt/templates/pages/default.php
@@ -3,4 +3,12 @@
 return [
     'extends' => 'belt-content::pages.web.show',
     'path' => 'belt-content::pages.templates.default',
+    'can_create_params' => true,
+    'params' => [
+        'flavor_text' => '',
+        'class' => [
+            'col-md-3' => 'default',
+            'col-md-12' => 'wide',
+        ],
+    ]
 ];
