6de5ed59a057a21180c78be86be49923a37f9378
Matt Crowe <github.mpc@zym.me>
2017-09-06 21:29:09 +0000

    'Builder' classes for Templateable items

--- config/belt/templates/pages/default.php
+++ config/belt/templates/pages/default.php
@@ -3,6 +3,7 @@
 return [
     'extends' => 'belt-content::pages.web.show',
     'path' => 'belt-content::pages.templates.default',
+    'builder' => \App\Builders\DefaultBuilder::class,
     'can_create_params' => true,
     'params' => [
         'flavor_text' => '',
