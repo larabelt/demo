6208355fa135b9211db226fad5190782fecdedd7
Matt Crowe <github.mpc@zym.me>
2017-09-12 18:08:58 +0000

    reload section params on template change

---
+++ config/belt/templates/attachments/hero.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'path' => 'belt-clip::attachments.sections.default',
+    'can_create_params' => false,
+    'params' => [
+        'class' => [
+            'col-md-3' => 'default',
+            'col-md-12' => 'wide',
+        ],
+    ]
+];
