54ba76fa7c4ce1d487dde1a9fac83cdeeb8631ce
Matt Crowe <github.mpc@zym.me>
2017-09-28 21:15:28 +0000

    tests

--- config/belt/search.php
+++ config/belt/search.php
@@ -3,6 +3,6 @@
 return [
     'classes' => [
         \Belt\Content\Page::class => \Belt\Content\Http\Requests\PaginatePages::class,
-        \Belt\Content\Post::class => \Belt\Content\Http\Requests\PaginatePosts::class,
+        \Belt\Spot\Place::class => \Belt\Spot\Http\Requests\PaginatePlaces::class,
     ]
 ];
