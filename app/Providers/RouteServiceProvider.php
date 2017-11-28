1fa3daf90377483133d953a24a6a83eb0662ccb9
Matt Crowe <github.mpc@zym.me>
2017-11-28 20:52:18 +0000

    composer update

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -40,8 +40,8 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
-        //include base_path('vendor/larabelt/core/routes/web/teams.php');
-        //include base_path('vendor/larabelt/core/routes/web/users.php');
+        include base_path('vendor/larabelt/core/routes/web/teams.php');
+        include base_path('vendor/larabelt/core/routes/web/users.php');
 
         /**
          * @larabelt
