1d46203f5755a6804338d625e10d881ac93f4598
Matt Crowe <github.mpc@zym.me>
2018-01-17 15:23:50 +0000

    admin home view

---
+++ resources/views/vendor/belt-core/base/admin/home.blade.php
@@ -0,0 +1,17 @@
+@extends('belt-core::layouts.admin.main')
+
+@section('heading-title', 'tmp')
+@section('heading-subtitle', 'tmp')
+@section('heading-active', 'tmp')
+
+@section('main')
+
+    <section class="content-header">
+        <h1><span>Welcome to LaraBelt Admin.</span><small></small></h1>
+    </section>
+    <section class="content">
+        <p>This view can be replaced in:</p>
+        <code>resources/views/vendor/belt-core/base/admin/home.blade.php</code>
+    </section>
+
+@stop
