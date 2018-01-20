7f9a88a84a04abbbeb50e2efc7d136e82f95ffae
Matt Crowe <github.mpc@zym.me>
2018-01-20 02:36:39 +0000

    init 1.5

--- resources/assets/js/belt-all.js
+++ resources/assets/js/belt-all.js
@@ -1,5 +1,3 @@
-import './belt-bootstrap';
-
 import BeltCore from 'belt/core/js/core';
 import BeltClip from 'belt/clip/js/clip';
 import BeltContent from 'belt/content/js/content';
@@ -11,6 +9,8 @@ import BeltSpot from 'belt/spot/js/spot';
  * Additional config options
  */
 window.larabelt.clip.accept = 'image/*,application/pdf,text/plain';
+
+Vue.config.devtools = process.env.MIX_VUEJS_DEBUG;
 
 $(document).ready(function () {
     new BeltCore([
