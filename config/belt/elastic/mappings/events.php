192df784d12037263fe92cda6523be564615400d
Matt Crowe <github.mpc@zym.me>
2018-01-22 16:49:44 +0000

    add priority trait to more spot models

--- config/belt/elastic/mappings/events.php
+++ config/belt/elastic/mappings/events.php
@@ -11,6 +11,7 @@ return [
         'meta_description' => \Belt\Content\Elastic\ElasticConfigHelper::property('text'),
         'meta_keywords' => \Belt\Content\Elastic\ElasticConfigHelper::property('text'),
         'meta_title' => \Belt\Content\Elastic\ElasticConfigHelper::property('text'),
+        'priority' => \Belt\Content\Elastic\ElasticConfigHelper::property('integer'),
         'searchable' => \Belt\Content\Elastic\ElasticConfigHelper::property('text'),
         'starts_at' => \Belt\Content\Elastic\ElasticConfigHelper::property('integer'),
         'ends_at' => \Belt\Content\Elastic\ElasticConfigHelper::property('integer'),
