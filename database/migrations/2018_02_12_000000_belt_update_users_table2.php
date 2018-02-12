0eaa6c7bbb4fb649c4be46f842ef7deb5867625e
Matt Crowe <github.mpc@zym.me>
2018-02-12 16:52:26 +0000

    users and team default to is_active = false

---
+++ database/migrations/2018_02_12_000000_belt_update_users_table2.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BeltUpdateUsersTable2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('is_active')->default(0)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('is_active')->default(1)->change();
+        });
+    }
+}
