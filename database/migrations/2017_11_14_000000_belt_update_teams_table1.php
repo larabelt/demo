21e84818245e0de5199f0c106b0f94ad54cbe2d6
Matt Crowe <github.mpc@zym.me>
2018-03-12 18:51:07 +0000

    alerts.show_url, alerts.url and alerts.intro

--- database/migrations/2017_11_14_000000_belt_update_teams_table1.php
+++ database/migrations/2017_11_14_000000_belt_update_teams_table1.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class BeltUpdateteamsTable1 extends Migration
+class BeltUpdateTeamsTable1 extends Migration
 {
     /**
      * Run the migrations.
