477a44f6eb8b62500bd2c63bd8d6bf68cb9cee38
Matt Crowe <github.mpc@zym.me>
2018-06-01 19:55:43 +0000

    tests

--- database/migrations/2018_05_25_000000_belt_create_list_items_table.php
+++ database/migrations/2018_05_25_000000_belt_create_list_items_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class BeltCreateListablesTable extends Migration
+class BeltCreateListItemsTable extends Migration
 {
     /**
      * Run the migrations.
