ee02bbe2d65a0ace792a713f67ac7662f74ddacd
Matt Crowe <github.mpc@zym.me>
2018-07-02 17:05:28 +0000

    tweak builder example class

--- app/Builders/DefaultBuilder.php
+++ app/Builders/DefaultBuilder.php
@@ -16,17 +16,7 @@ class DefaultBuilder extends BaseBuilder
 {
     public function build()
     {
-//        $attachment = Attachment::query()->orderBy('id', 'DESC')->first();
-//
-//        $this->section([
-//            'sectionable_type' => $attachment->getMorphClass(),
-//            'sectionable_id' => $attachment->id,
-//        ]);
 
-        $this->section([
-            'heading' => 'Page Section Title',
-            'before' => 'Main Page Section Content',
-        ]);
     }
 
 }
