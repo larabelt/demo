3853e835731518abb62dbb8e3477ded48f086a45
Matt Crowe <github.mpc@zym.me>
2018-07-14 01:52:16 +0000

    belt updates

---
+++ config/belt/templates/attachments/default.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    // The human-readable name of your template.
+    'label' => '',
+
+    // A short description of template.
+    'description' => '',
+
+    // A blade layout that show can show a snapshot of what the templates structure and/or style will look like when compiled.
+    'preview' => '',
+
+    /*
+    | A set of custom parameters that belong to the templatable object.
+    |
+    | Each parameters has the following configuration options:
+    |
+    | @type:        Required. The type of input to be used in the admin UX,
+    |               ie: text, textarea, select, editor or other properly added custom values.
+    |
+    | @label:       The human-readable name of the parameter.
+    |
+    | @description: A short description of parameter.
+    |
+    | @options:     The list of available options where type="select". Option keys are machine-readable.
+    |               Option values will be used as human-readable labels.
+    */
+
+    'params' => [
+
+    ],
+
+];
