40cebd27ef3925c4aae434a81924e082155bc0ab
Matt Crowe <github.mpc@zym.me>
2018-07-19 20:48:29 +0000

    init refactor template to subtype. refactor UpdateService

---
+++ database/migrations/2018_07_19_000000_belt_content_update_subtypes.php
@@ -0,0 +1,50 @@
+<?php
+
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BeltContentUpdateSubtypes extends Migration
+{
+    protected $tables = [
+        'attachments',
+        'blocks',
+        'lists',
+        'list_items',
+        'pages',
+        'posts',
+        'sections',
+        'terms',
+    ];
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach ($this->tables as $table) {
+            Schema::table($table, function (Blueprint $table) {
+                $table->renameColumn('template', 'subtype');
+            });
+            DB::statement("ALTER TABLE $table MODIFY COLUMN `subtype` VARCHAR(255) AFTER `id`");
+        }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        foreach ($this->tables as $table) {
+            Schema::table($table, function (Blueprint $table) {
+                $table->renameColumn('subtype', 'template');
+            });
+        }
+    }
+}
